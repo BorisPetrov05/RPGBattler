70c6ca69bed6dd75a2c5f05bc8d537506a6a4c9e
Boris Petrov <boris05_petrov@abv.bg>
2026-05-13 16:47:24 +0300

    Updated Character.h

--- Character.h
+++ Character.h
@@ -1,1 +1,48 @@
 #pragma once
+#include "MyString.hpp"
+
+enum class CharacterType
+{
+	Warrior,
+	Mage,
+	Archer
+};
+
+class Character
+{
+private:
+	MyString name;
+	CharacterType type;
+
+	int maxHP;
+	int currentHP;
+
+	int level = 1;
+
+	int minDamage;
+	int maxDamage;
+
+public:
+	Character();
+	Character(const MyString& name, CharacterType type, int maxHP, int level, int minDamage, int maxDamage);
+	Character(const Character& other);
+	Character(Character&& other) noexcept;
+
+	int attack() const;
+	void takeDamage(int amount);
+	void heal(int amount);
+	void resetHP();
+	bool isAlive() const;
+	void levelUp(int choice);
+	void UseAbility() const;
+	void clone() const;
+
+	MyString getName() const;
+	CharacterType getType() const;
+	int getMaxHP() const;
+	int getCurrentHP() const;
+	int getLevel() const;
+	int getMinDamage() const;
+	int getMaxDamage() const;
+
+};
